8a6f12ed3ab3435909c6ecd9a175bbc041a68971
iqwrwq <Arthur.schimpf@gmx.de>
2022-03-26 01:40:32 +0100

    fix: replaced component

---
+++ components/FlipCard.js
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import memoryCardCover from "../public/memoryCards/memoryCard-cover.png";
+import memoryCardSuccess from "../public/memoryCards/memoryCard-success.png";
+import ReactCardFlip from "react-card-flip";
+import {useEffect, useState} from "react";
+
+
+export default function FlipCard(props) {
+    const [flip, setFlip] = useState(true);
+    const doFlip = (e) => {
+        setFlip(true);
+        e.preventDefault();
+        props.handleClick(props.card, setFlip);
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            setFlip(false);
+        }, 2000)
+    }, [])
+
+    return (
+        <div onClick={doFlip}>
+            {props.card.pop ?
+                <Image className="successCard" src={memoryCardSuccess}/> :
+                <ReactCardFlip isFlipped={flip}>
+                    <Image src={memoryCardCover} key='front'/>
+                    <Image src={props.card.image} key='back'/>
+                </ReactCardFlip>
+            }
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import memoryCard7 from '../public/memoryCards/memoryCard-7.png'
 import memoryCard8 from '../public/memoryCards/memoryCard-8.png'
 
 import {useState} from "react";
-import FlipCard from "./api/FlipCard";
+import FlipCard from "../components/FlipCard";
 
 export default function Home() {
     const data = [
