89365daff9411078b84de2c8e8f2b60cebbaa38b
iqwrwq <Arthur.schimpf@gmx.de>
2022-03-26 01:29:22 +0100

    feat: memory game first version

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import memoryCard1 from '../public/memoryCards/memoryCard-1.png'
 import memoryCard2 from '../public/memoryCards/memoryCard-2.png'
 import memoryCard3 from '../public/memoryCards/memoryCard-3.png'
@@ -7,60 +6,86 @@ import memoryCard5 from '../public/memoryCards/memoryCard-5.png'
 import memoryCard6 from '../public/memoryCards/memoryCard-6.png'
 import memoryCard7 from '../public/memoryCards/memoryCard-7.png'
 import memoryCard8 from '../public/memoryCards/memoryCard-8.png'
-import memoryCardCover from '../public/memoryCards/memoryCard-cover.png'
 
-import ReactCardFlip from "react-card-flip";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import FlipCard from "../components/FlipCard";
 
 export default function Home() {
-    const memoryCards = [
-        memoryCard1, memoryCard1,
-        memoryCard1, memoryCard1,
-        memoryCard1, memoryCard1,
-        memoryCard1, memoryCard1,
-        memoryCard1, memoryCard1,
-        memoryCard1, memoryCard1,
-        memoryCard1, memoryCard1,
-        memoryCard1, memoryCard1
-    ]
+    const data = [
+        {index: 0, id: 1, image: memoryCard1, pop: false}, {index: 8, id: 1, image: memoryCard1, pop: false},
+        {index: 1, id: 2, image: memoryCard2, pop: false}, {index: 9, id: 2, image: memoryCard2, pop: false},
+        {index: 2, id: 3, image: memoryCard3, pop: false}, {index: 10, id: 3, image: memoryCard3, pop: false},
+        {index: 3, id: 4, image: memoryCard4, pop: false}, {index: 11, id: 4, image: memoryCard4, pop: false},
+        {index: 4, id: 5, image: memoryCard5, pop: false}, {index: 12, id: 5, image: memoryCard5, pop: false},
+        {index: 5, id: 6, image: memoryCard6, pop: false}, {index: 13, id: 6, image: memoryCard6, pop: false},
+        {index: 6, id: 7, image: memoryCard7, pop: false}, {index: 14, id: 7, image: memoryCard7, pop: false},
+        {index: 7, id: 8, image: memoryCard8, pop: false}, {index: 15, id: 8, image: memoryCard8, pop: false}
+    ];
 
-    const shuffleArray = (array) => {
-        let currentIndex = array.length, randomIndex;
+    const [score, setScore] = useState(0);
+    const [matchIsOver, setMatchOver] = useState(false);
+    const [cachedCard, setCachedCard] = useState({card: null, callback: null});
+    const [hasFirstCardFlipped, setHasFirstCardFlipped] = useState(false);
+    const [memoryCards, setMemoryCards] = useState(shuffle(data));
 
-        while (currentIndex != 0) {
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex--;
+    function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
 
-            [array[currentIndex], array[randomIndex]] = [
-                array[randomIndex], array[currentIndex]];
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
         }
-
         return array;
     }
-    const [shuffle, doShuffle] = useState(shuffleArray(memoryCards))
 
-    const [flip, setFlip] = useState(false);
-    const handleClick = (e) => {
-        e.preventDefault();
-        setFlip(!flip);
+    const handleClick = (card, callback) => {
+        if (hasFirstCardFlipped && cachedCard.card.index !== card.index && cachedCard.card.pop !== true && card.pop !== true) {
+            if (cardsMatch(cachedCard.card, card)) {
+                setScore(score + 1);
+                setTimeout(() => {
+                    cachedCard.card.pop = true;
+                    card.pop = true;
+                    if (memoryCards.every(card => card.pop === true)) {
+                        console.log("match is over")
+                        setMatchOver(true);
+                    }
+                }, 500)
+            }
+            setHasFirstCardFlipped(false);
+            setCachedCard(null);
+            setTimeout(() => {
+                cachedCard.callback(false);
+                callback(false);
+            }, 1000)
+        } else {
+            setCachedCard({card, callback});
+            setHasFirstCardFlipped(true);
+        }
     }
 
+    const cardsMatch = (card1, card2) => {
+        return card1.id === card2.id;
+    }
+
+    const resetGame = () => {
+        window.location.reload(false);
+    }
 
     return (
         <div>
             <div className="scoreboard">
-                <div className="score"></div>
+                <div className="score">{score}</div>
+                <button className='btn' onClick={resetGame}>reset</button>
             </div>
-            <div className='game'>
-                {memoryCards.map((card, index) => (
-                    <div onClick={handleClick} key={index}>
-                        <ReactCardFlip isFlipped={flip} >
-                            <Image src={memoryCardCover} key='front'/>
-                            <Image src={card} key='back'/>
-                        </ReactCardFlip>
-                    </div>
-                ))}
-            </div>
+            {matchIsOver ?
+                <button className='btn center' onClick={resetGame}>reset</button> :
+                <div className='center game'>
+                    {memoryCards.map((card, index) => (
+                        <FlipCard key={index} handleClick={handleClick} card={card}/>
+                    ))}
+                </div>
+            }
         </div>
     )
 }
---
+++ pages/win.js
@@ -0,0 +1,7 @@
+export default function win(){
+    return(
+        <div className="center">
+            Hello
+        </div>
+    );
+}
